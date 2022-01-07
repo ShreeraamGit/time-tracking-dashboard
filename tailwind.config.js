a32f10bf439c8fb201118fe04c54728cf930161e
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-01-07 13:26:29 +0100

    profile section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'page-background': '#0C1323',
+        'work':'#35397B',
+        'play':'#1E204A',
+        'study':'#20214B',
+        'excercise':'#4AD082',
+        'social':'#7234D2',
+        'selfcare':'#F1C75A',
+        'profile':'#5746E9',
+        'timings':'#1E204A'
+      },
+      fontFamily:{
+        'main':['Rubik']
+      }
+    },
   },
   plugins: [],
 }
